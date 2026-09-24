a969e0b68b673b840f40bc21fb64630b9dbf8cda
Linnéa <linnea.ternevik@gmail.com>
2026-09-24 16:49:28 +0200

    Add color variables, fonts and global styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
 import router from "./routes/router";
+import "./styles/variables.css";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
