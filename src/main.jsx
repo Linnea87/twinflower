fefc06113ddfc238d8b4b59d5051d0c3e37ae8f7
Linnéa <linnea.ternevik@gmail.com>
2026-09-24 22:03:58 +0200

    Add Redux store and Provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router";
+import store from "./store/store";
 import router from "./routes/router";
 import "./styles/variables.css";
 import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>,
 );
